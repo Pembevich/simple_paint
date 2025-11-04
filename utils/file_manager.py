f1a20f4c4e8ad6ab481236fc62269f4b9761d373
Pembevich <vladpianist24@gmail.com>
2025-11-04 18:02:14 +0300

    Create file_manager.py

---
+++ utils/file_manager.py
@@ -0,0 +1,9 @@
+class FileManager:
+    def __init__(self):
+        pass
+    
+    def save_image(self, image, filename):
+        print(f"Сохранение в файл: {filename}")
+    
+    def load_image(self, filename):
+        print(f"Загрузка из файла: {filename}")
