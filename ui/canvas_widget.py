49f09f99039e2af1200da1af4497291069f42279
Pembevich <vladpianist24@gmail.com>
2025-11-13 10:29:24 +0300

    Update canvas_widget.py

--- ui/canvas_widget.py
+++ ui/canvas_widget.py
@@ -16,33 +16,55 @@ class CanvasWidget(QWidget):
         self.current_tool = BrushTool()
         self.current_color = QColor(0, 0, 0)
         self.brush_size = 5
+        self.temp_image = None  # Для временного предпросмотра
 
     def paintEvent(self, event):
         painter = QPainter(self)
+        # Рисуем основное изображение
         painter.drawImage(self.rect(), self.image, self.rect())
+        
+        # Если рисуем фигуру - рисуем временный предпросмотр
+        if self.drawing and self.temp_image:
+            painter.drawImage(self.rect(), self.temp_image, self.rect())
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
             self.drawing = True
             self.last_point = event.pos()
             self.start_point = event.pos()
+            # Создаем временное изображение для предпросмотра
+            self.temp_image = QImage(self.size(), QImage.Format.Format_ARGB32)
+            self.temp_image.fill(Qt.GlobalColor.transparent)
 
     def mouseMoveEvent(self, event):
         if self.drawing:
-            painter = QPainter(self.image)
+            # Для кисти и ластика рисуем сразу
             if self.current_tool.name in ["brush", "eraser"]:
+                painter = QPainter(self.image)
                 self.current_tool.draw(painter, self.last_point, event.pos(), 
                                      self.current_color, self.brush_size)
                 self.last_point = event.pos()
                 self.update()
+            # Для фигур - рисуем предпросмотр
+            else:
+                # Очищаем временное изображение
+                self.temp_image.fill(Qt.GlobalColor.transparent)
+                painter = QPainter(self.temp_image)
+                # Рисуем фигуру на временном изображении
+                self.current_tool.draw(painter, self.start_point, event.pos(),
+                                     self.current_color, self.brush_size)
+                self.update()
 
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton and self.drawing:
+            # Для фигур - рисуем окончательно на основном изображении
             if self.current_tool.name not in ["brush", "eraser"]:
                 painter = QPainter(self.image)
                 self.current_tool.draw(painter, self.start_point, event.pos(),
                                      self.current_color, self.brush_size)
-                self.update()
+            # Очищаем временное изображение
+            self.temp_image = None
+            self.update()
             self.drawing = False
 
     def clear(self):
