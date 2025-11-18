be575ac45850f592520c4c01c68e5c1a7bc1e2c9
Pembevich <vladpianist24@gmail.com>
2025-11-18 11:08:26 +0300

    Update canvas_widget.py

--- ui/canvas_widget.py
+++ ui/canvas_widget.py
@@ -1,8 +1,7 @@
 from PyQt6.QtWidgets import QWidget
 from PyQt6.QtCore import Qt, QPoint
 from PyQt6.QtGui import QPainter, QColor, QPen, QImage
-from models.drawing_tools import BrushTool, LineTool, RectangleTool, EllipseTool, EraserTool
-
+from models.drawing_tools import BrushTool, LineTool, RectangleTool, EllipseTool, EraserTool, FillTool
 
 class CanvasWidget(QWidget):
     def __init__(self):
@@ -16,14 +15,12 @@ class CanvasWidget(QWidget):
         self.current_tool = BrushTool()
         self.current_color = QColor(0, 0, 0)
         self.brush_size = 5
-        self.temp_image = None  # Для временного предпросмотра
+        self.temp_image = None
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        # Рисуем основное изображение
         painter.drawImage(self.rect(), self.image, self.rect())
         
-        # Если рисуем фигуру - рисуем временный предпросмотр
         if self.drawing and self.temp_image:
             painter.drawImage(self.rect(), self.temp_image, self.rect())
 
@@ -32,40 +29,49 @@ class CanvasWidget(QWidget):
             self.drawing = True
             self.last_point = event.pos()
             self.start_point = event.pos()
-            # Создаем временное изображение для предпросмотра
-            self.temp_image = QImage(self.size(), QImage.Format.Format_ARGB32)
-            self.temp_image.fill(Qt.GlobalColor.transparent)
+            
+            # Для заливки - выполняем сразу при клике
+            if self.current_tool.name == "fill":
+                self.perform_fill(event.pos())
+                self.drawing = False
+            else:
+                self.temp_image = QImage(self.size(), QImage.Format.Format_ARGB32)
+                self.temp_image.fill(Qt.GlobalColor.transparent)
 
     def mouseMoveEvent(self, event):
-        if self.drawing:
-            # Для кисти и ластика рисуем сразу
+        if self.drawing and self.current_tool.name != "fill":
             if self.current_tool.name in ["brush", "eraser"]:
                 painter = QPainter(self.image)
                 self.current_tool.draw(painter, self.last_point, event.pos(), 
                                      self.current_color, self.brush_size)
                 self.last_point = event.pos()
                 self.update()
-            # Для фигур - рисуем предпросмотр
             else:
-                # Очищаем временное изображение
                 self.temp_image.fill(Qt.GlobalColor.transparent)
                 painter = QPainter(self.temp_image)
-                # Рисуем фигуру на временном изображении
                 self.current_tool.draw(painter, self.start_point, event.pos(),
                                      self.current_color, self.brush_size)
                 self.update()
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.MouseButton.LeftButton and self.drawing:
-            # Для фигур - рисуем окончательно на основном изображении
+        if (event.button() == Qt.MouseButton.LeftButton and 
+            self.drawing and self.current_tool.name != "fill"):
             if self.current_tool.name not in ["brush", "eraser"]:
                 painter = QPainter(self.image)
                 self.current_tool.draw(painter, self.start_point, event.pos(),
                                      self.current_color, self.brush_size)
-            # Очищаем временное изображение
             self.temp_image = None
             self.update()
             self.drawing = False
+
+    def perform_fill(self, point):
+        """Выполняет заливку области"""
+        if self.current_tool.name == "fill" and hasattr(self.current_tool, 'flood_fill'):
+            success = self.current_tool.flood_fill(self.image, point, self.current_color)
+            if success:
+                self.update()
+            else:
+                print("Заливка не выполнена")
 
     def clear(self):
         self.image.fill(Qt.GlobalColor.white)
